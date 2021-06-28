26927febad35c446ddcd8e0ef019451d3d89b240
Tom Oinn <tomoinn@crypticsquid.com>
2021-06-28 12:54:01 +0100

    Updated example data provider to use nginx_cert_parser from main code rather than creating custom one

--- src/docs/ec2/data_provider.py
+++ src/docs/ec2/data_provider.py
@@ -1,24 +1,12 @@
 import logging
 
 import flask
-from cryptography import x509
-from cryptography.hazmat.backends import default_backend
 
-from ib1.openenergy.support import AccessTokenValidator
+from ib1.openenergy.support import AccessTokenValidator, nginx_cert_parser
 
 LOG = logging.getLogger('example.energydata.org.uk')
 
 logging.basicConfig(level=logging.INFO)
-
-
-def nginx_cert_parser():
-    """
-    Pull certificate out of X-OE-CLIENT-CERT header from nginx
-    """
-    cert_str = flask.request.headers['X-OE-CLIENT-CERT'].replace('\t', '')
-    LOG.info(f'found cert in header \n{cert_str}')
-    return x509.load_pem_x509_certificate(cert_str.encode('ASCII'), default_backend())
-
 
 validator = AccessTokenValidator(client_id='olQh4BLV0mQUaM3OZbpXy',
                                  certificate='/home/ec2-user/certs/a.pem',
